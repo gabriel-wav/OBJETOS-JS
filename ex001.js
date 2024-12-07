cc561fe48737774c6d8d359227078287e272e8b7
gabriel-wav <gabfer.silva@gmail.com>
2024-12-06 21:04:21 -0300

    feito exercicios

---
+++ ex001.js
@@ -0,0 +1,10 @@
+//Você tem um objeto livro com as propriedades titulo: "O Senhor dos Anéis", autor: "J.R.R. Tolkien", e ano: 1954. A tarefa é acessar e exibir o título do livro e o autor usando a notação de ponto.
+
+let livro = {
+  titulo: "O senhor dos Anéis",
+  autor: "J.R.R. Tolkien",
+  ano: 1954,
+};
+
+console.log(livro.titulo);
+console.log(livro.autor);
---
+++ ex002.js
@@ -0,0 +1,12 @@
+//Você tem um objeto carro com as propriedades marca: "Honda", modelo: "Civic", e ano: 2020. Altere o valor da propriedade ano para 2022 e adicione uma nova propriedade chamada cor com o valor "Prata". Exiba o objeto atualizado.
+
+let carro = {
+    marca: "Honda",
+    modelo: "Civic",
+    ano: 2020
+}
+
+carro.ano = 2022
+carro.cor = "Prata"
+
+console.log (carro)  
